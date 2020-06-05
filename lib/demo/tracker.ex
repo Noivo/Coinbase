0b85a64edd92e22f60908baa477f6cffe2d3a1f5
jmnoivo <jmnoivo@gmail.com>
2020-06-05 18:12:33 +0100

    Remove some function wasn't being used

--- lib/demo/tracker.ex
+++ lib/demo/tracker.ex
@@ -14,14 +14,6 @@ defmodule Demo.Tracker do
     GenServer.call(__MODULE__, :history, 25000)
   end
 
-  def coin_ticker(ticker) do
-    GenServer.call(__MODULE__, {:coin_ticker, ticker})
-  end
-
-  def all_conversions do
-    GenServer.call(__MODULE__, :all_conversions)
-  end
-
   # Private API
   def init(state) do
     send(self(), :work)
@@ -32,28 +24,17 @@ defmodule Demo.Tracker do
     {:reply, state, state}
   end
 
-  def handle_call({:coin_ticker, ticker}, _, state) do
-    {:ok, coin} = Coinbase.product(ticker)
-    state = [%{ticker => coin}] ++ state
-    {:reply, coin, state}
-  end
-
-  def handle_call(:all_conversions, _, [head | _] = state) do
-    {:reply, head, state}
-  end
-
   def handle_info(:work, state) do
     state =
       with {:ok, result} <- Coinbase.products() do
         [result] ++ state
       else
-        error -> 
-          IO.inspect(error)  
-          state        
+        _ -> state
       end
+
     head = List.first(state)
 
-    broadcast({:ok, head}, :history) 
+    broadcast({:ok, head}, :history)
     Process.send_after(self(), :work, 5000)
     {:noreply, state}
   end
@@ -65,6 +46,4 @@ defmodule Demo.Tracker do
   defp broadcast({:ok, result}, event) do
     Phoenix.PubSub.broadcast(Demo.PubSub, @topic, {event, result})
   end
-
-  defp broadcast({:error, _reason} = error, _event), do: error
 end
